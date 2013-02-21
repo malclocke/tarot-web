24a7fcae4cf18a465421913f29df13873ebf7ca4
Malcolm Locke <malc@wholemeal.co.nz>
2013-02-21 20:08:20 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TarotWeb::Application.config.secret_token = 'ea374d65a33d251290dc1565410d15b6a3248822013732688f325405736484a9f7dd3b3ae05a541e015931fea815cb69ad321ce2a4634df8b71b115b744ee71f'
