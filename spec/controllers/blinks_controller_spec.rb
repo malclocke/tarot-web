17cf9b234b2dd8a11e92e0475cc1c69dae984e2e
Malcolm Locke <malc@wholemeal.co.nz>
2013-05-28 10:10:49 +1200

    Fix specs for BlinksController

--- spec/controllers/blinks_controller_spec.rb
+++ spec/controllers/blinks_controller_spec.rb
@@ -4,8 +4,16 @@ describe BlinksController do
 
   describe '#index' do
 
+    let(:reference)   { mock('reference') }
+    let(:night_image) { mock('night_image', :reference => reference) }
+
+    before do
+      NightImage.stub(:next => night_image)
+    end
+
     specify do
       get :index
+      assigns[:night_image].should == night_image
       response.should render_template('index')
     end
 
